5316869f6e8707223cfc7a7e9a1d57b100b37a5b
Andre Souto <soutoandre@gmail.com>
2019-03-14 18:12:16 -0300

    Update server.js
    rollback to alemanha

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-res.send('Hello Alemanha!');
+res.send('Hello Brasil!');
 });
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
